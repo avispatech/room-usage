b971175baafde3666b1130520acb347783eb0054
Leonardo Luarte <leonardo@luarte.net>
2021-02-07 14:31:40 -0300

    oops forgot the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :days, only: %i[show index]
+  resources :shifts, only: :create
+
+  root to: 'days#index'
 end
